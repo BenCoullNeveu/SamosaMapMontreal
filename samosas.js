4373ebe48ca6df6224ca8826cc9db122d21d443e
CoullObservatory <114324520+CoullObservatory@users.noreply.github.com>
2025-08-13 15:56:50 -0400

    Added details and Pushap

--- samosas.js
+++ samosas.js
@@ -30,12 +30,23 @@ const samosas = [
     mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.5123,-73.5672"
   },
   {
-    name: "MUHC Glen site Samosas\n(find cafe name)",
+    name: "Comptoir Veritas (MUHC Glen site)",
     lat: 45.474, 
     lng: -73.601,
     description: "Decent samosas available at the MUHC Glen site. Chutney is not great, but the samosas are good. Sold in a set of 2 samosas per order.\n\nEasiest to get to from the metro tunnel connecting the hospital to Vendome metro.\n\nThe cafe is located on the first floor (up a flight of stairs from the metro entrance).",
     rating: 3.0,
     cost: "$1.55 / samosa",
+    hours: "Monday-Friday: 07:00-20:00",
     mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.474,-73.601"
+  },
+  {
+    name: "Pushap\nNeed to try :D",
+    lat: 45.49759,
+    lng: -73.65389,
+    description: "To update after trying ;).\nSamosas are sold in a set of 2",
+    rating: -1,
+    cost: "$1.00 / samosa",
+    hours: "Monday-Sunday: 11:30-21:00",
+    mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.49759,-73.65389"
   }
 ];
