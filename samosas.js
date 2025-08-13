7f536feb783af4bce6f511b47036c7a303881ce1
CoullObservatory <114324520+CoullObservatory@users.noreply.github.com>
2025-08-13 14:05:39 -0400

    Updated locations

--- samosas.js
+++ samosas.js
@@ -1,29 +1,41 @@
 const samosas = [
   {
-    name: "Bombay Mahal",
-    lat: 45.5351,
-    lng: -73.6132,
-    description: "Classic Indian eatery with tasty and affordable samosas.",
+    name: "Bombay Mahal (NDG)",
+    lat: 45.4697,
+    lng: -73.6172,
+    description: "Hard dough and sub-par chutney.",
+    rating: 3.0,
+    cost: "$2.50 / samosa",
+    hours: "Wednesday-Monday: 11:00-22:00",
+    mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.4697,-73.6172"
+  },
+  {
+    name: "Ganges",
+    lat: 45.4679,
+    lng: -73.6212,
+    description: "Great samosas, though I'm probably biased because these were the first samosas I ever had in Montreal. Sold as a set of 2. \nThey have a great selection of Indian food, and the samosas are always fresh.",
     rating: 4.5,
-    cost: "$",
-    mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.5351,-73.6132"
+    cost: "~ $2.48 / samosa",
+    hours: "Monday-Sunday: 14:00-22:00",
+    mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.4679,-73.6212"
   },
   {
-    name: "Pushap Restaurant",
-    lat: 45.5146,
-    lng: -73.6530,
-    description: "Vegetarian Indian restaurant with amazing samosas.",
-    rating: 4.4,
-    cost: "$",
-    mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.5146,-73.6530"
+    name: "Eva B (thrift store)",
+    lat: 45.5123,
+    lng: -73.5672,
+    description: "A thrift store with a cafe that serves samosas. The samosas are very good, each coming with a fairly good mango chutney. \nThe cafe is located on the main floor, near the main entrance.",
+    rating: 4.5,
+    cost: "~ $1.50 / samosa",
+    hours: "Monday-Saturday: 11:00-19:00, Sunday: 12:00-18:00",
+    mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.5123,-73.5672"
   },
   {
-    name: "Le Taj",
-    lat: 45.5049,
-    lng: -73.5736,
-    description: "Upscale Indian dining with gourmet samosas.",
-    rating: 4.3,
-    cost: "$$$",
-    mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.5049,-73.5736"
+    name: "MUHC Glen site Samosas (find cafe name)",
+    lat: 45.474, 
+    lng: -73.601,
+    description: "Decent samosas available at the MUHC Glen site. Chutney is not great, but the samosas are good. Sold in a set of 2 samosas per order. \nEasiest to get to from the metro tunnel connecting the hospital to Vendome metro. The cafe is located on the first floor (up a flight of stairs from the metro entrance).",
+    rating: 3.0,
+    cost: "$1.55 / samosa",
+    mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.474,-73.601"
   }
 ];
