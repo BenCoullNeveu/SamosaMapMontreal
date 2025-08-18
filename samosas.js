3031da4c88d27f44b4a0b7450d17284d653dc396
CoullObservatory <114324520+CoullObservatory@users.noreply.github.com>
2025-08-18 09:24:43 -0400

    Edited links and pushap{

--- samosas.js
+++ samosas.js
@@ -7,7 +7,7 @@ const samosas = [
     rating: 3.0,
     cost: "$2.50 / samosa",
     hours: "Wednesday-Monday: 11:00-22:00",
-    mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.4697,-73.6172"
+    mapsLink: "https://www.google.com/maps/place/Bombay+Mahal/@45.4696819,-73.6197503,17z/data=!3m1!4b1!4m6!3m5!1s0x4cc91199bebd911b:0xf5c8f89b68145506!8m2!3d45.4696819!4d-73.6171754!16s%2Fg%2F11gyyv7bpy?entry=ttu&g_ep=EgoyMDI1MDgxMy4wIKXMDSoASAFQAw%3D%3D"
   },
   {
     name: "Ganges",
@@ -17,7 +17,7 @@ const samosas = [
     rating: 4.5,
     cost: "~ $2.48 / samosa",
     hours: "Monday-Sunday: 14:00-22:00",
-    mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.4679,-73.6212"
+    mapsLink: "https://www.google.com/maps/place/Ganges+Restaurant/@45.4679265,-73.6238293,16z/data=!3m1!4b1!4m6!3m5!1s0x4cc910b44a6eca8b:0x54d65666bcbc596b!8m2!3d45.4679265!4d-73.6212544!16s%2Fg%2F1tdx89vv?entry=ttu&g_ep=EgoyMDI1MDgxMy4wIKXMDSoASAFQAw%3D%3D"
   },
   {
     name: "Eva B (thrift store)",
@@ -27,7 +27,7 @@ const samosas = [
     rating: 4.5,
     cost: "~ $1.50 / samosa",
     hours: "Monday-Saturday: 11:00-19:00, Sunday: 12:00-18:00",
-    mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.5123,-73.5672"
+    mapsLink: "https://www.google.com/maps/place/Eva+B./@45.5115543,-73.5697334,17z/data=!3m2!4b1!5s0x4cc91a4bf58488ed:0x89011ed4e7dc455d!4m6!3m5!1s0x4cc91a4bf584fa0f:0x29e2151da8a6069c!8m2!3d45.5115543!4d-73.5671585!16s%2Fg%2F1td6g2pm?entry=ttu&g_ep=EgoyMDI1MDgxMy4wIKXMDSoASAFQAw%3D%3D"
   },
   {
     name: "Comptoir Veritas (MUHC Glen site)",
@@ -37,16 +37,16 @@ const samosas = [
     rating: 3.0,
     cost: "$1.55 / samosa",
     hours: "Monday-Friday: 07:00-20:00",
-    mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.474,-73.601"
+    mapsLink: "https://www.google.com/maps/place/Comptoir+Veritas+Cafe/@45.4733308,-73.6034275,17z/data=!3m2!4b1!5s0x4cc910a33d0f33b5:0x6637b6e7de08e084!4m6!3m5!1s0x4cc910a31687f9ed:0x27e81e2834ebd1d9!8m2!3d45.4733308!4d-73.6008526!16s%2Fg%2F11f1wd1xhx?entry=ttu&g_ep=EgoyMDI1MDgxMy4wIKXMDSoASAFQAw%3D%3D"
   },
   {
-    name: "Pushap\nNeed to try :D",
+    name: "Pushap",
     lat: 45.49759,
     lng: -73.65389,
-    description: "To update after trying ;).\nSamosas are sold in a set of 2",
-    rating: -1,
-    cost: "$1.00 / samosa",
+    description: "Samosas are sold in a set of 2, or in a set of 12 for $10.\nEasily the best samosas, with a perfect combination of crispiness and tenderness throughout. Simple flavours, but makes it so that no flavour is too overwhelming. Tamarind chutney is very good and complements the samosas very well.",
+    rating: 5.0,
+    cost: "$1.50 / samosa, or $0.83 / samosa (for 12 samosas)",
     hours: "Monday-Sunday: 11:30-21:00",
-    mapsLink: "https://www.google.com/maps/dir/?api=1&destination=45.49759,-73.65389"
+    mapsLink: "https://www.google.com/maps/place/Pushap/@45.4975815,-73.6564598,17z/data=!3m1!4b1!4m6!3m5!1s0x4cc919d1bddd5deb:0xab48a6c739d6dce9!8m2!3d45.4975815!4d-73.6538849!16s%2Fg%2F1w4f7qjs?entry=ttu&g_ep=EgoyMDI1MDgxMy4wIKXMDSoASAFQAw%3D%3D"
   }
 ];
